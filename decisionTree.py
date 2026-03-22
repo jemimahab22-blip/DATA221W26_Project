60b84944c7d6df8e75479d87a8b24cf7b54eedbe
jemimahab22-blip <jemimahab22@gmail.com>
2026-03-22 06:20:11 -0600

    -added the skip bad images

--- decisionTree.py
+++ decisionTree.py
@@ -7,22 +7,18 @@ from sklearn.tree import DecisionTreeClassifier
 #We now have to convert the images into features since the Decision Tree Classifier does not work well with images according the research.
 #Decision trees need tabular data not images.
 
-#here, I am getting the dataset from the path
-dataset_path = "C:/Users/jemim/OneDrive - University of Calgary/Desktop/DATA 221/Pneumonia-Chest Xray/chest_xray"
-
-train_path= os.path.join(dataset_path)  #I am getting the train path using the dataset_path
-
-#print(os.listdir(train_path))  #this prints out what is contained in the train_path
-
-def load_data(dataset_path, label):
+def load_data(folder_path, label):
     data =[]
     labels =[]
 
-    for image_name in train_path:  #I am iterating through the train path to find the image names
-        image_path= os.path.join(dataset_path, image_name)
+    for image_name in os.listdir(folder_path):  #I am iterating through the train path to find the image names
+        image_path= os.path.join(folder_path, image_name)
 
         image = cv2.imread(image_path)   #I am reading the image_path
-        image = cv2.resize(image, (200,200))   #resizing the image
+        if image is None:
+            continue  #this skips bad images
+
+        image = cv2.resize(image, (100,100))   #resizing the image
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #converting to the grayscale
 
         image = image.flatten() #Flattening the image to 1D
@@ -31,7 +27,3 @@ def load_data(dataset_path, label):
         labels.append(label)
 
     return data, labels
-
-
-
-
