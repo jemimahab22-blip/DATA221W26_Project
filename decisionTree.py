2487e003aa39caebc35eab99474a7ef4d3055eac
jemimahab22-blip <jemimahab22@gmail.com>
2026-03-22 04:44:41 -0600

    -accessed the dataset_path using os.path.join

--- decisionTree.py
+++ decisionTree.py
@@ -7,7 +7,10 @@ import cv2
 #Decision trees need tabular data not images.
 
 #here, I am getting the dataset from the path
-dataset_path = "C:/Users/jemim/OneDrive - University of Calgary/Desktop/DATA 221/Pneumonia-Chest Xray.zip"
+dataset_path = "C:/Users/jemim/OneDrive - University of Calgary/Desktop/DATA 221/Pneumonia-Chest Xray/chest_xray"
 
+train_path= os.path.join(dataset_path, "train")  #I am getting the train path using the dataset_path
+
+print(os.listdir(train_path))  #this prints out what is contained in the train_path
 
 
