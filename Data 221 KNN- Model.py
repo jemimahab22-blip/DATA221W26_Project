01bd392682583db86a8856a3fe8328d68bf32726
Hadia-Rahman <Hadia.rahman@ucalgary.ca>
2026-03-24 12:39:41 -0600

    Added more import statements and TODOs

--- Data 221 KNN- Model.py
+++ Data 221 KNN- Model.py
@@ -1,7 +1,18 @@
 # import statements
 import kagglehub
+from sklearn.neighbors import KNeighborsClassifier
+import cv2
+import numpy as np
 
 # Download the latest version of the dateset
 path = kagglehub.dataset_download("paultimothymooney/chest-xray-pneumonia")
 
 print("Path to dataset files:", path)
+
+# TODO: Change image into vector to work for KNN (RESIZE + FLATTEN)
+
+# TODO: Convert to arrays
+
+# TODO: Train test split
+
+# TODO: Apply KNN
