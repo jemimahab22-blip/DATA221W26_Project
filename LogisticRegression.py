b887a1bd75b000684fda6bb7f30c09336f988ace
arishma-lalani <arishma.lalani@ucalgary.ca>
2026-03-25 12:56:31 -0600

    Download dataset and configure

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -9,3 +9,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import (accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix)
 
+# Download the dataset and configure it
+download_dataset_path = kagglehub.dataset_download("paultimothymooney/chest-xray-pneumonia")
+print("Path to dataset files:", download_dataset_path)
+
+dataset_image_resized = 100
+data_from_flattened_images_in_dataset = []
+labels_from_flattened_images_in_dataset = []
