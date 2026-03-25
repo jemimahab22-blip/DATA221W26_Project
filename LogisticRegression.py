2d77269e374d5a1bf43dc4c5eade86d7723bf11f
arishma-lalani <arishma.lalani@ucalgary.ca>
2026-03-25 13:38:34 -0600

    Make confusion matrix more readable

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -124,6 +124,10 @@ print('~' * 30)
 # Confusion matrix to see where the model failed to correctly classify the images
 pneumonia_confusion_matrix = confusion_matrix(labels_test, test_predictions)
 
+plt.figure(figsize=(6, 4))
+# Make confusion matrix readable
+sns.heatmap(pneumonia_confusion_matrix, annot=True, fmt="d", cmap='Blues', xticklabels=['Normal', 'Pneumonia'], yticklabels=['Normal', 'Pneumonia'])
+
 plt.title('Logistic Regression (Baseline) Confusion Matrix')
 plt.ylabel('True label')
 plt.xlabel('Predicted label')
