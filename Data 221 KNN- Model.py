712d12d46a009beb232d8352477002fd76dbce3a
Hadia-Rahman <Hadia.rahman@ucalgary.ca>
2026-03-25 01:09:24 -0600

    Added Logic to TODO Load the dataset and define the subfolder path

--- Data 221 KNN- Model.py
+++ Data 221 KNN- Model.py
@@ -1,13 +1,22 @@
 # import statements
+
 import kagglehub
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import os
 import cv2
 import numpy as np
 
-# Download the latest version of the dateset
-path = kagglehub.dataset_download("paultimothymooney/chest-xray-pneumonia")
+# TODO: Load the dataset and define the subfolder path
 
+path = kagglehub.dataset_download("paultimothymooney/chest-xray-pneumonia")
 print("Path to dataset files:", path)
+train_path = os.path.join(path, "chest_xray", "train")
+data_from_images_in_dataset= []
+labels_from_images_in_dataset = []
+
+IMAGE_SIZE = 100
 
 # TODO: Change image into vector to work for KNN (RESIZE + FLATTEN)
 
